e008a791c987273381fb704db92809040241c404
Brian Zauner <binrz00@hotmail.com>
2021-11-30 17:40:45 -0600

    added pics and disapparing descriptions

---
+++ script.js
@@ -0,0 +1,49 @@
+const home = document.getElementById("home");
+const homeDesc = document.getElementById("homeDesc");
+const life = document.getElementById("life");
+const lifeDesc = document.getElementById("lifeDesc");
+const auto = document.getElementById("auto");
+const autoDesc = document.getElementById("autoDesc");
+const health = document.getElementById("health");
+const healthDesc = document.getElementById("healthDesc");
+home.addEventListener("click", function () {
+    if (homeDesc.attributes.length === 2) {
+        homeDesc.removeAttribute("hidden")
+        return
+    }
+    if (homeDesc.attributes.length === 1) {
+        homeDesc.setAttribute("hidden", true)
+    }
+
+});
+
+life.addEventListener("click", function () {
+    if (lifeDesc.attributes.length === 2) {
+        lifeDesc.removeAttribute("hidden")
+        return
+    }
+    if (lifeDesc.attributes.length === 1) {
+        lifeDesc.setAttribute("hidden", true)
+    }
+
+});
+auto.addEventListener("click", function () {
+    if (autoDesc.attributes.length === 2) {
+        autoDesc.removeAttribute("hidden")
+        return
+    }
+    if (autoDesc.attributes.length === 1) {
+        autoDesc.setAttribute("hidden", true)
+    }
+
+});
+health.addEventListener("click", function () {
+    if (healthDesc.attributes.length === 2) {
+        healthDesc.removeAttribute("hidden")
+        return
+    }
+    if (healthDesc.attributes.length === 1) {
+        healthDesc.setAttribute("hidden", true)
+    }
+
+});
